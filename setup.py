7bf05c140051626d5d1b8799c9551798feab8743
alanzhan <luocheng@uw.edu>
2018-11-19 10:38:26 -0800

    manifest

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+import os
 import setuptools
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename)) 
+    return paths
+
+extra_files = package_files('/n_data')
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -13,9 +23,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Luochenghuang/shared_lib",
     packages=setuptools.find_packages(),
+    package_data={'', extra_files},
     classifiers=[
         "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: Linux",
     ],
+    include_package_data=True,
 )
