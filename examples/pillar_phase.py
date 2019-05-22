243c585f6370f538630e952ba6598115007a6d77
Luochenghuang <35114355+Luochenghuang@users.noreply.github.com>
2019-05-22 12:57:38 -0700

    Update pillar_phase.py
    Slighted modified example

--- examples/pillar_phase.py
+++ examples/pillar_phase.py
@@ -3,45 +3,38 @@ from rcwa.cscs import *
 import numpy as np
 import os
 
-
+# Directory info
 Dir = 'test'
 Name = '1'
 
-
-# configuration
-wavelengths = [0.633, 0.7] # has to be in an array
-periodicity = 0.7*0.633 # optimized for 560 nm
+# Basic configurations
+wavelengths = [0.633, 0.7]    # has to be in an array
+periodicity = 0.7 * 0.633     # optimized for 560 nm
 grat_tkn = 1
 buff_tkn = 0.633
 dbr_Si3N4 = 0.633 / 4.0 / 2.
 dbr_SiO2  = 0.633 / 4.0 / 1.457
 
 aspect_ratio = 6
-resolution = 
+resolution = 100
 
-
-# don't worry about
-max_r = periodicity/2.
-min_r = grat_tkn/float(aspect_ratio)/2.
+# Variables for the simulations
+max_r = periodicity / 2.
+min_r = grat_tkn / float(aspect_ratio) / 2.
 r_list = np.linspace(min_r, max_r, num=resolution)
 
-
-# make a directory
+# Make a directory
 directory = Dir + '/'
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-subdivision = 1
-
-
-# start RCWA
+# Start RCWA
 inputs = []
 for w in wavelengths:
     for r in r_list:
         inpt = 'r-{}-({},0.),(0.,{})/TiO2={}:C(0,0,{})/SiO2={}/Ag=1.1'\
           .format(w,periodicity,periodicity,grat_tkn,r,dbr_SiO2,dbr_Si3N4)
         inputs.append(inpt)
-
 
 new = RCWA(inputs, 19, field=1)
 df = new.simulate()
